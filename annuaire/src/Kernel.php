eef9290f95d509622807f58ebbdaccebc5445485
SandraMILLET <sandra.millet11@gmail.com>
2020-04-15 11:06:56 +0200

    reset

--- annuaire/src/Kernel.php
+++ annuaire/src/Kernel.php
@@ -8,8 +8,6 @@ use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
-use Symfony\Component\HttpFoundation\File\File;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 class Kernel extends BaseKernel
 {
